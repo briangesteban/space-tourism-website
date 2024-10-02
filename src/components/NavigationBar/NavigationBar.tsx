bc903cfa7ae29dcfae7364156c7308089bcf359e
Brian Esteban <briangesteban@gmail.com>
2024-10-01 17:29:11 -0700

    Layout navigation bar and add hero section and navigation bar styling

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+import Logo from "../../assets/shared/logo.svg";
+import BurgerIcon from "../../assets/shared/icon-hamburger.svg";
 import "./NavigationBar.scss";
 
 const NavigationBar = () => {
-  return <></>;
+  return (
+    <nav className="nav-bar">
+      <Link to="/home" className="nav-bar__link">
+        <img src={Logo} alt="Space Tourism Logo" className="nav-bar__logo" />
+      </Link>
+      <button
+        className="nav-bar__btn-burger"
+        onClick={() => {
+          console.log(true);
+        }}
+      >
+        <img
+          src={BurgerIcon}
+          alt="Open Navigation Menu"
+          className="nav-bar__btn-icon"
+        />
+      </button>
+    </nav>
+  );
 };
 
 export { NavigationBar };
