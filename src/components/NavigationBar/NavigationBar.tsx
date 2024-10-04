e1cfc6779ea1cdc9d6fe2f686d914a9a1db41e68
Brian Esteban <briangesteban@gmail.com>
2024-10-04 16:21:34 -0700

    Update burger menu styling, functionality, and animation

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -13,7 +13,8 @@ const NavigationBar = () => {
       <button
         className="nav-bar__btn-burger"
         onClick={() => {
-          console.log(true);
+          const navigation = document.querySelector(".nav-bar__menu");
+          navigation?.classList.add("nav-bar__menu--open");
         }}
       >
         <img
@@ -22,32 +23,42 @@ const NavigationBar = () => {
           className="nav-bar__btn-icon"
         />
       </button>
-      <div className="nav-bar__overlay"></div>
       <aside className="nav-bar__menu">
-        <div className="nav-bar__btn-wrapper">
-          <button
-            className="nav-bar__btn-close"
-            onClick={() => console.log("test")}
+        <div className="nav-bar__overlay"></div>
+        <div className="nav-bar__navigation">
+          <div className="nav-bar__btn-wrapper">
+            <button
+              className="nav-bar__btn-close"
+              onClick={() => {
+                const navigation = document.querySelector(".nav-bar__menu");
+                navigation?.classList.remove("nav-bar__menu--open");
+              }}
+            >
+              <img
+                src={CloseIcon}
+                alt="Close Navigation Menu"
+                className="nav-bar__btn-icon"
+              />
+            </button>
+          </div>
+          <Link
+            to="/"
+            className="nav-bar__navigation-link nav-bar__navigation-link--active"
           >
-            <img
-              src={CloseIcon}
-              alt="Close Navigation Menu"
-              className="nav-bar__btn-icon"
-            />
-          </button>
+            <span className="nav-bar__navigation-link--number">00</span> Home
+          </Link>
+          <Link to="/" className="nav-bar__navigation-link">
+            <span className="nav-bar__navigation-link--number">01</span>{" "}
+            Destination
+          </Link>
+          <Link to="/" className="nav-bar__navigation-link">
+            <span className="nav-bar__navigation-link--number">02</span> Crew
+          </Link>
+          <Link to="/" className="nav-bar__navigation-link">
+            <span className="nav-bar__navigation-link--number">03</span>{" "}
+            Technology
+          </Link>
         </div>
-        <Link to="/" className="nav-bar__menu-link nav-bar__menu-link--active">
-          <span className="nav-bar__menu-link--number">00</span> Home
-        </Link>
-        <Link to="/" className="nav-bar__menu-link">
-          <span className="nav-bar__menu-link--number">01</span> Destination
-        </Link>
-        <Link to="/" className="nav-bar__menu-link">
-          <span className="nav-bar__menu-link--number">02</span> Crew
-        </Link>
-        <Link to="/" className="nav-bar__menu-link">
-          <span className="nav-bar__menu-link--number">03</span> Technology
-        </Link>
       </aside>
     </nav>
   );
