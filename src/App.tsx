cc72eac2ddf18c73a24b54e7789add5edbd3b57c
Brian Esteban <briangesteban@gmail.com>
2024-10-07 17:28:12 -0700

    Add false pointer event on body on navigation open state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useSelector } from "react-redux";
+import { RootState } from "./utils/redux/state/store";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { NavigationBar } from "./components/NavigationBar/NavigationBar";
 import { HomePage } from "./pages/HomePage";
 import "./App.scss";
 
 const App = () => {
+  const EL_BODY: HTMLElement | null = document.getElementById("root");
+  const navState: boolean = useSelector(
+    (state: RootState): boolean => state.isNavOpen.value
+  );
+
+  // If Navigation Menu is open, Pointer Events on Body Element turns false.
+  if (navState) {
+    EL_BODY?.classList.add("pointer-evt--false");
+  } else {
+    EL_BODY?.classList.remove("pointer-evt--false");
+  }
+
   return (
     <BrowserRouter>
       <NavigationBar />
